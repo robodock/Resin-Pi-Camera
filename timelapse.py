cd421085d50725035cfe4646b0daea24af089b72
Eddie <eddie@robodock.net>
2015-05-29 15:25:22 +0800

    20150529 15:25

--- timelapse.py
+++ timelapse.py
@@ -2,18 +2,10 @@
 
 import time
 import picamera
-from datetime import datetime, timedelta
-
-def wait():
-    # Calculate the delay to the start of the next hour
-    next_hour = (datetime.now() + timedelta(hours=1)).replace(
-        minute=0, second=0, microsecond=0)
-    delay = (next_hour - datetime.now()).seconds
-    time.sleep(delay)
 
 with picamera.PiCamera() as camera:
     camera.start_preview()
-    wait()
-    for filename in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M}.jpg'):
+    time.sleep(2)
+    for filename in camera.capture_continuous('img{counter:04d}.jpg'):
         print('Captured %s' % filename)
-        wait()
+        time.sleep(10) # wait 10 seconds
