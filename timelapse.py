afe22ca6ab2ba71675d572ce35e46fe0eb25abc8
Eddie <eddie@robodock.net>
2015-05-29 16:12:13 +0800

    20150529 16:12

--- timelapse.py
+++ timelapse.py
@@ -6,6 +6,6 @@ import picamera
 with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
-    for filename in camera.capture_continuous('img{counter:04d}.jpg'):
+    for filename in camera.capture_continuous('/data/img{counter:04d}.jpg'):
         print('Captured %s' % filename)
         time.sleep(10) # wait 10 seconds
