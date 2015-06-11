60fe06f9401da072ac83bcdeae5e6d84dc5c4de9
Eddie <eddie@robodock.net>
2015-06-11 16:07:25 +0800

    20150611 16:07

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ export PASSWD=${PASSWD:=root}
 echo "root:$PASSWD" | chpasswd
 dropbear -E -F &
 
-python /app/timelapse2.py
+python /app/timelapse.py
--- timelapse.py
+++ timelapse.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import os, subprocess, datetime, shutil
+import os, datetime, shutil
 import picamera
 
 #time_interval = os.environ["INTERVAL"]
 
 dataDir="/data"
-pictureDir="capture"
+pictureDir=dataDir + "/capture")
 
 os.chdir(dataDir)
 
@@ -18,31 +18,34 @@ def find_oldest_dir(path) :
 	return oldestDir
 
 def diskusage(path) :
-	df = subprocess.Popen(["du", path], stdout=subprocess.PIPE)
-	return df.communicate()[0]
+	st = os.statvfs(path)
+	free = st.f_bavail * st.f_frsize
+	#total = st.f_blocks * st.f_frsize
+	#used = (st.f_blocks - st.f_bfree) * st.f_frsize
+	return free 
 
 	
 if not os.path.exists(pictureDir) :
 	os.makedirs(pictureDir)
 
-# set disk splace limit to 5GB
-max=5000000
+# set disk splace limit to 500MB
+min=500000
 
 while True :
 # check disk space used already
-	while diskusage(pictureDir) >= max :
+	while diskusage(pictureDir) <= min :
 		shutil.rmtree(find_oldest_dir(pictureDir))
 
 	currentHour=datetime.datetime.now().strftime("%Y%m%d_%H")
-	if not os.path.exists(currentHour) :
-		os.makedirs(currentHour)
+	if not os.path.exists(os.path.join(pictureDir, currentHour)) :
+		os.makedirs(os.path.join(pictureDir, currentHour))
 		
 	with picamera.PiCamera() as camera:
 		camera.resolution=(os.environ["IMAGE_WIDTH"], os.environ["IMAGE_HEIGH"])
 		camera.rotation=(270)
 	#camera.start_preview()
 	#time.sleep(2)
-		camera.capture(os.path.join(dataDir,pictureDir,currentHour)+datetime.datetime.now().strftime("%Y%M%d-%H%M%S")+'.jpg')
+		camera.capture(os.path.join(dataDir,pictureDir,currentHour)+'/'+datetime.datetime.now().strftime("%Y%M%d-%H%M%S")+'.jpg')
 	
 	#	for filename in camera.capture_continuous('/data/img-{timestamp:%Y%m%d-%H%M%S}.png'):
 	#	print('Captured %s' % filename)
