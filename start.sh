ab8e0affd48f3a3b8869e5b635db2b48d062d4e5
robodock <eddie@robodock.net>
2015-06-14 18:06:52 +0800

    20150614 18:06

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ export PASSWD=${PASSWD:=root}
 echo "root:$PASSWD" | chpasswd
 dropbear -E -F &
 
-#python /app/timelapse.py
-python /app/Hello.py
+python /app/timelapse.py
+#python /app/Hello.py
--- timelapse.py
+++ timelapse.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python
+f#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 13 16:21:33 2015
-Raspberry Pi timelapse camera for Resin.io
+Raspberry Pi timelapse camera using Resin.io
 picture save into hourly folder in /data/capture,
 remove oldest hour when disk full.
 get picture resolution from Environment variables IMAGE_WIDTH, IMAGE_HEIGHT
+get picture rotation from Environment variables ROTATE
 get timelaspe interval from Environment variables TIME_INTERVAL
 @author: eddie@robodock.net
 """
@@ -20,15 +21,14 @@ pictureDir=dataDir + "/capture"
 
 def find_oldest_dir(path) :
 	os.chdir(path)
-	oldestDir = min(os.listdir(path), key=os.path.getctime)
-	return oldestDir
+	return sorted(os.listdir(path), key=os.path.getctime)[0]
 
 def diskusage(path) :
 	st = os.statvfs(path)
-	free = st.f_bavail * st.f_frsize
+	return st.f_bavail * st.f_frsize
+	#free = st.f_bavail * st.f_frsize
 	#total = st.f_blocks * st.f_frsize
 	#used = (st.f_blocks - st.f_bfree) * st.f_frsize
-	return free 
 
 
 os.chdir(dataDir)
@@ -42,14 +42,14 @@ min=500000000
 while True :
 
 	while diskusage(pictureDir) <= min :
-		shutil.rmtree(find_oldest_dir(pictureDir))
+		shutil.rmtree(os.path.join(pictureDir, find_oldest_dir(pictureDir)))
 
 	currentHour=datetime.datetime.now().strftime("%Y%m%d_%H")
 	if not os.path.exists(os.path.join(pictureDir, currentHour)) :
 		os.makedirs(os.path.join(pictureDir, currentHour))
 		
 	with picamera.PiCamera() as camera:
-		camera.resolution=(int(os.environ["IMAGE_WIDTH"]), int(os.environ["IMAGE_HEIGH"]))
-		camera.rotation=(270) #my camera angle
+		camera.resolution=(int(os.environ.get("IMAGE_WIDTH", 800)), int(os.environ.get("IMAGE_HEIGH", 600)))
+		camera.rotation=(int(os.environ.get("ROTATE", 270))) #my camera angle
 		camera.capture(os.path.join(pictureDir,currentHour)+'/'+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.png')
-		time.sleep(float(os.environ["TIME_INTERVAL"])) # wait time_interval seconds
+		time.sleep(float(os.environ.get("TIME_INTERVAL", 1)) ) # wait time_interval seconds
